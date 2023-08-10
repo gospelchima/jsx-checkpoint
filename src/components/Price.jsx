2ce87efb5c7ba3dd0dd6e3bef98b0543735ae03b
gospelchima <gospelnkem@gmail.com>
2023-08-10 14:17:05 +0100

    latest

--- src/components/Price.jsx
+++ src/components/Price.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Product from "../product";
 
 const Price = () => {
-  return <div>&#8358;{Product.price.toLocaleString()}</div>;
+  return <div style={{marginBottom:"10px"}}>&#8358;{Product.price.toLocaleString()}</div>;
 };
 
 export default Price;
