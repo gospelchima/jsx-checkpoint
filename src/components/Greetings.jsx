54def584b040f240c54c75ca6b718291344c097a
gospelchima <gospelnkem@gmail.com>
2023-08-10 14:02:31 +0100

    updated

--- src/components/Greetings.jsx
+++ src/components/Greetings.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
-import waveimg from '../assets/hand.png'
+import React from "react";
+import waveimg from "../assets/hand.png";
 
-const Greetings = ({Name}) => {
+const Greetings = ({ Name }) => {
   return (
-    <div>Hello, {Name?Name:"there!"}
-    {Name&&<img src={waveimg} style={{width:"20px", height:"20px"}}/>}
+    <div style={{ marginTop:"15px"}}>
+      Hello, {Name ? Name : "there!"}
+      {Name && (
+        <img
+          src={waveimg}
+          style={{ width: "20px", height: "20px", marginLeft: "10px"}}
+        />
+      )}
     </div>
-  )
-}
-
-export default Greetings
+  );
+};
+export default Greetings;
